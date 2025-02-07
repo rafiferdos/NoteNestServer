267bb5c29ae3dc49d780228437d3b00e8d0e039f
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-07 21:33:36 +0600

    set up Express application with global error handling and not found middleware

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-// import stationaryRouter fromm './app/modules/stationary/stationary.route'
+import notFound from './app/middlewares/notFound'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
+//TODO import router from './app/routes'
+
+const app: Application = express()
+
+app.use(express.json())
+app.use(cors())
+
+// Application Routes
+//TODO app.use('/api', router)
+const getController = async (req: Request, res: Response) => {
+  res.send("Whats Up Buddy!")
+}
+
+app.get('/', getController)
+app.use(globalErrorHandler)
+app.use(notFound)
+
+export default app
