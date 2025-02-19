e56a6e578a967ecce7295dc9e8b6166239e490f4
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-19 13:52:11 +0600

    add product controller with methods for creating and retrieving products

---
+++ src/app/modules/product/product.controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import catchAsync from "../../utils/catchAsync";
+import { VProduct } from "./product.validation";
+import { SProduct } from "./product.service";
+import sendResponse from "../../utils/sendResponse";
+import { StatusCodes } from "http-status-codes";
+
+const cCreateProduct = catchAsync(async (req, res) => {
+  const validatedData = VProduct.vCreateProductSchema.parse(req.body)
+  const product = await SProduct.sCreateProduct(validatedData)
+
+  sendResponse(res, {
+    statusCode: StatusCodes.CREATED,
+    success: true,
+    message: 'Product created successfully',
+    data: product,
+  })
+})
+
+const cGetAllProducts = catchAsync(async (req, res) => {
+  const searchTerm = req.query.searchTerm as string
+  const products = await SProduct.sGetAllProducts(searchTerm)
+  
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Products fetched successfully',
+    data: products,
+  })
+})
+
