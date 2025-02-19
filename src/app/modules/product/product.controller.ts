8cfdbf2b49c01dcfc8dc982de6c0d2b810811386
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-19 14:02:04 +0600

    add update and delete methods to product controller

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -48,3 +48,55 @@ const cGetProductById = catchAsync(async (req, res) => {
     data: product,
   })
 })
+
+const cUpdateProduct = catchAsync(async (req, res) => {
+  const validatedData = VProduct.vUpdateProductSchema.parse(req.body)
+  const product = await SProduct.sUpdateProduct(
+    req.params.productId,
+    validatedData,
+  )
+
+  if (!product) {
+    sendResponse(res, {
+      statusCode: StatusCodes.NOT_FOUND,
+      success: false,
+      message: 'Product not found',
+    })
+    return
+  }
+
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Product updated successfully',
+    data: product,
+  })
+})
+
+const cDeleteProduct = catchAsync(async (req, res) => {
+  const product = await SProduct.sDeleteProduct(req.params.productId)
+
+  if (!product) {
+    sendResponse(res, {
+      statusCode: StatusCodes.NOT_FOUND,
+      success: false,
+      message: 'Product not found',
+    })
+    return
+  }
+
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Product deleted successfully',
+    data: product,
+  })
+})
+
+export const CProduct = {
+  cCreateProduct,
+  cGetAllProducts,
+  cGetProductById,
+  cUpdateProduct,
+  cDeleteProduct,
+}
