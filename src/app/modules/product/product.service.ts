2bbcb410927a62cb58b94a6e02bab949b563de37
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-19 12:21:14 +0600

    add product service methods for creation and retrieval of products

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -1,9 +1,26 @@
 import { IProduct } from "./product.interface";
+import { MProduct } from "./product.model";
 
-const createProduct = async(
+const sCreateProduct = async(
   productData: Partial<IProduct>,
 ): Promise<IProduct> => {
   const product = new MProduct(productData)
   await product.save()
   return product
 }
+
+const sGetAllProducts = async(
+  searchTerm?: string,
+): Promise<IProduct[]> => {
+  const query = searchTerm
+    ? {
+      $or: [
+        { name: searchTerm },
+        { brand: searchTerm },
+        { category: searchTerm },
+    ]
+    }
+    : {}
+  return await MProduct.find(query)
+}
+
