a187df1844c3ff2aca4bdc7bda202f19d8dac5f7
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-19 12:07:54 +0600

    add validation schemas for product creation and update using Zod

---
+++ src/app/modules/product/product.validation.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod'
+import { ProductCategory } from './product.interface'
+
+const vCreateProductSchema = z.object({
+  name: z.string().nonempty('Name is required'),
+  brand: z.string().nonempty('Brand is required'),
+  price: z.number().positive('Price must be positive'),
+  category: z.nativeEnum(ProductCategory, { message: 'Invalid category' }),
+  description: z.string().nonempty('Description is required'),
+  quantity: z.number().positive('Quantity must be positive'),
+  inStock: z.boolean(),
+})
+
+const vUpdateProductSchema = z.object({
+  name: z.string().optional(),
+  brand: z.string().optional(),
+  price: z.number().positive('Price must be positive').optional(),
+  category: z
+    .nativeEnum(ProductCategory, { message: 'Invalid category' })
+    .optional(),
+  description: z.string().optional(),
+  quantity: z.number().positive('Quantity must be positive').optional(),
+  inStock: z.boolean().optional(),
+})
+
+export const VProduct = {
+  vCreateProductSchema,
+  vUpdateProductSchema,
+}
