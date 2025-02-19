dab3ee6fd56750539d607af2b1a4ca1b7fab947e
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-19 11:52:44 +0600

    implement routing by enabling application routes in the main app file

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import notFound from './app/middlewares/notFound'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
-//TODO import router from './app/routes'
+import router from './app/routes'
 
 const app: Application = express()
 
@@ -10,9 +10,9 @@ app.use(express.json())
 app.use(cors())
 
 // Application Routes
-//TODO app.use('/api', router)
+app.use('/api', router)
 const getController = async (req: Request, res: Response) => {
-  res.send("Whats Up Buddy!")
+  res.send('Whats Up Buddy!')
 }
 
 app.get('/', getController)
