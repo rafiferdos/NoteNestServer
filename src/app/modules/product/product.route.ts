d9bad43dcce8afeaf96109734e504fc054caaf41
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-23 17:27:07 +0600

    feat: add authentication middleware to product routes for admin access

--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express'
 import { CProduct } from './product.controller'
+import auth from '../../middlewares/auth'
 
 const router = Router()
 
-router.post('/', CProduct.cCreateProduct)
+router.post('/', auth('admin'), CProduct.cCreateProduct)
 
 router.get('/', CProduct.cGetAllProducts)
 
 router.get('/:productId', CProduct.cGetProductById)
 
-router.put('/:productId', CProduct.cUpdateProduct)
+router.put('/:productId', auth('admin'), CProduct.cUpdateProduct)
 
-router.delete('/:productId', CProduct.cDeleteProduct)
+router.delete('/:productId', auth('admin'), CProduct.cDeleteProduct)
 
 export const RProduct = router
