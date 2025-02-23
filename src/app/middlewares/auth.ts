0f4c86e6f66b5a713fcd87ff57a644504fed43dc
Rafi Ferdos <rafiferdos@gmail.com>
2025-02-23 17:43:51 +0600

    fix: improve authorization middleware to handle missing token scenario

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,14 +2,19 @@ import { NextFunction, Request, Response } from 'express'
 import jwt, { JwtPayload } from 'jsonwebtoken'
 import config from '../config'
 import AppError from '../errors/AppError'
+import { StatusCodes } from 'http-status-codes'
 const auth = (...requiredRoles: string[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       const extractedToken = req.headers.authorization
-      const token = (extractedToken as string).split(' ')[1]
-      //   console.log("Token===: ", token);
 
-      //if the token is sent from the client
+      if (!extractedToken) {
+        throw new AppError(
+          StatusCodes.UNAUTHORIZED,
+          'You are not authorized as admin',
+        )
+      }
+      const token = (extractedToken as string).split(' ')[1]
       if (!token) {
         throw new AppError(401, 'You are not Authorized')
       }
